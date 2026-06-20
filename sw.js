7f7876a457281a8ac0169310fb0b0394914e7867
hunter2342 <m.rabie.general@gmail.com>
2026-06-20 14:33:27 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+});
+self.addEventListener('fetch', (e) => {
+    // تفعيل التخزين المؤقت المستقبلي (Cache)
+});
